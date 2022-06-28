f14c95acd064438acdd7d3afdd917951c4798d83
purveshmakode24 <makodepurvesh@gmail.com>
2022-06-28 19:13:57 +0530

    Add Insert, Update, Delete Db operations

--- services/sql/DBOperations.py
+++ services/sql/DBOperations.py
@@ -13,24 +13,19 @@ class DBOperations:
         try:
             connection = ConnectionManager.DBConnection().getConnection()
             cursor = connection.cursor()
-
             # if params_map is None:
             #     cursor.execute(query)
             # else:
             #     cursor.execute(query, params_map)
-
             cursor.execute(query, params)
-
             if cursor.rowcount > 0:
                 """cursor.fetchall() ->
                 Fetch all rows of a query result set
                 and returns a list of tuples.
                 If no more rows are available, it returns an empty list."""
                 result = cursor.fetchall()
-
         except Exception as e:
-            log.info("Some execption occured:"+str(e))
-
+            log.error("Some execption occured:"+str(e))
         finally:
             if connection is not None:
                 ConnectionManager.DBConnection().close(connection)
@@ -39,11 +34,59 @@ class DBOperations:
         If no more rows are available, it returns Null."""
         return result
 
-    def runInsertQuery(self, query, params_map=None):
-        pass
+    def runInsertQuery(self, query, params):
+        """Execute Insert Query"""
+        result = True
+        connection = None
+        try:
+            connection = ConnectionManager.DBConnection().getConnection()
+            cursor = connection.cursor()
+            cursor.execute(query, params)
+            connection.commit()
+        except Exception as e:
+            log.error("Some execption occured:"+str(e))
+            result = False
+        finally:
+            if connection is not None:
+                ConnectionManager.DBConnection().close(connection)
+        """Returns True if Insert Query successfully executed,
+        else it returns False."""
+        return result
 
-    def runUpdateQuery(self, query, params_map=None):
-        pass
+    def runUpdateQuery(self, query, params):
+        """Execute Update Query"""
+        result = True
+        connection = None
+        try:
+            connection = ConnectionManager.DBConnection().getConnection()
+            cursor = connection.cursor()
+            cursor.execute(query, params)
+            connection.commit()
+        except Exception as e:
+            log.error("Some execption occured:"+str(e))
+            result = False
+        finally:
+            if connection is not None:
+                ConnectionManager.DBConnection().close(connection)
+        """Returns True if Update Query successfully executed,
+        else it returns False."""
+        return result
 
-    def runDeleteQuery(self, query, params_map=None):
-        pass
+    def runDeleteQuery(self, query, params):
+        """Execute Delete Query"""
+        result = True
+        connection = None
+        try:
+            connection = ConnectionManager.DBConnection().getConnection()
+            cursor = connection.cursor()
+            cursor.execute(query, params)
+            connection.commit()
+        except Exception as e:
+            log.error("Some execption occured:"+str(e))
+            result = False
+        finally:
+            if connection is not None:
+                ConnectionManager.DBConnection().close(connection)
+        """Returns True if Delete Query successfully executed,
+        else it returns False."""
+        return result

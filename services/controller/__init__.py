276f4a8b0b4795deb6d74706525419b2328a5982
purveshmakode24 <makodepurvesh@gmail.com>
2022-07-10 16:36:47 +0530

    Update title & description

--- services/controller/__init__.py
+++ services/controller/__init__.py
@@ -2,8 +2,9 @@ from flask_restx import Api
 from controller.ProductsController import ns as products_ns
 
 api = Api(
-    title='API',
+    title='Bulletproof Flask API',
     version='1.0',
-    description='Test API')
+    description='A simple, scalable, and powerful architecture \
+    for building production ready Flask application API services.')
 
 api.add_namespace(products_ns, '/service1')
